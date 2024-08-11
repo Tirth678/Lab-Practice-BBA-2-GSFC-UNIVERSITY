a2659ee12232e82e7052704f748629ed13ac1163
Tirth S Pandya <teamfznltd@gmail.com>
2024-08-11 14:13:15 +0530

    commit

--- 23.c
+++ 23.c
@@ -1,16 +1,11 @@
-#include<stdio.h>
-int main(){
-    int burger=10;
-    float result;
-    do
-    {   
-        result *= 0.10;
-        printf("%f\n", result);
-        result++;
-        burger++;
-    } while (burger<5);
-    
-// will solve it later
-
+#include <stdio.h>
+int main() {
+    float price = 10.0; 
+    float increase = 0.1;
+    int years = 5;
+    for (int i = 0; i < years; i++) {
+        price *= (1 + increase); // Increase the price by 10%
+    }
+    printf("The price of the burger after 5 years is: %.2f\n", price);
     return 0;
 }
